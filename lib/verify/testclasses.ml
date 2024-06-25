0e6f5f5f179a3233840e487223a75c290145b6e6
Emilia Kond <emilia@rymiel.space>
2024-06-25 15:47:18 +0300

    Make classloading output more concise

--- lib/verify/testclasses.ml
+++ lib/verify/testclasses.ml
@@ -11,18 +11,18 @@ let class_sub_dirs = [ "lib/"; "test/"; "extern/" ] (* in order of priority *)
 let test_loader (name : string) : jclass =
   let make_path subdir = (class_base_dir ^ subdir ^ name ^ ".class", subdir) in
   let candidate_paths = List.map make_path class_sub_dirs in
-  Printf.printf "\027[33m! bootstrap class loader is loading %S\027[0m\n" name;
+  Printf.printf "\027[33m! bootstrap loading %S\027[0m..." name;
   let find_class (path, subdir) =
     if Sys.file_exists path then Some (parse path, subdir) else None
   in
   let found_class = List.find_map find_class candidate_paths in
   match found_class with
   | Some (cls, path) ->
-      Printf.printf "  \027[33m! %S was loaded from %S\027[0m\n" name path;
+      Printf.printf "\t\027[33mfound in %S\027[0m\n" path;
       cls
   | None ->
       Printf.printf
-        "\027[31mClass %S not found. Searched the following locations: %s\027[0m\n"
+        "\n\027[31mClass %S not found. Searched the following locations: %s\027[0m\n"
         name
         (String.concat ", "
            (List.map
