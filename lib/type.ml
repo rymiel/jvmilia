3b3ff0aa1b113bcbedae1affc6e25178aaed7c58
Emilia Kond <emilia@rymiel.space>
2024-07-22 03:06:56 +0300

    Remove deprecated type methods

--- lib/type.ml
+++ lib/type.ml
@@ -116,58 +116,14 @@ let rec parse_dtype (s : string) (offset : int ref) : dtype =
       Object classname
   | c -> failwith (Printf.sprintf "Invalid descriptor %c" c)
 
-let rec parse_vtype (s : string) (offset : int ref) : vtype =
-  match parse_arraytype s offset with
-  | T x -> x
-  | Byte | Char | Short | Boolean -> Int
-
-and parse_arraytype (s : string) (offset : int ref) : arraytype =
-  let c = String.get s !offset in
-  incr offset;
-  match c with
-  | 'D' -> T Double
-  | 'F' -> T Float
-  | 'J' -> T Long
-  | 'I' -> T Int
-  | 'B' -> Byte
-  | 'C' -> Char
-  | 'S' -> Short
-  | 'Z' -> Boolean
-  | 'V' -> T Void
-  | '[' -> T (Array (parse_arraytype s offset))
-  | 'L' ->
-      let count = ref 0 in
-      while String.get s (!offset + !count) <> ';' do
-        incr count
-      done;
-      let classname = String.sub s !offset !count in
-      offset := !offset + !count + 1;
-
-      T (Class (classname, Bootstrap))
-  | c -> failwith (Printf.sprintf "Invalid descriptor %c" c)
-
 let parse_class_internal_name (s : string) : vtype =
   if String.starts_with ~prefix:"[" s then (
     let offset = ref 0 in
-    let t = parse_vtype s offset in
+    let t = parse_dtype s offset in
     assert (!offset = String.length s);
-    match t with Array _ -> t | _ -> failwith "Not an array type?"
+    vtype_of_dtype t
     (* kissing owner this a good idea *))
   else Class (s, Bootstrap)
-
-let parse_method_descriptor (desc : string) : vtype list * vtype =
-  let s = desc in
-  assert (String.get s 0 = '(');
-  let offset = ref 1 in
-  let args = ref [] in
-  while String.get s !offset <> ')' do
-    let t = parse_vtype s offset in
-    args := !args @ [ t ]
-  done;
-  incr offset;
-  let ret = parse_vtype s offset in
-  assert (!offset = String.length desc);
-  (!args, ret)
 
 let parse_method_descriptord (desc : string) : dtype list * dtype =
   let s = desc in
@@ -183,12 +139,6 @@ let parse_method_descriptord (desc : string) : dtype list * dtype =
   assert (!offset = String.length desc);
   (!args, ret)
 
-let parse_field_descriptor (desc : string) : vtype =
-  let offset = ref 0 in
-  let t = parse_vtype desc offset in
-  assert (!offset = String.length desc);
-  t
-
 let parse_field_descriptord (desc : string) : dtype =
   let offset = ref 0 in
   let t = parse_dtype desc offset in
@@ -197,11 +147,6 @@ let parse_field_descriptord (desc : string) : dtype =
 
 let map_vtype_method ((args, ret) : dtype list * dtype) : vtype list * vtype =
   (List.map vtype_of_dtype args, vtype_of_dtype ret)
-
-let (parse_vtype [@deprecated]) = parse_vtype
-let (parse_arraytype [@deprecated]) = parse_arraytype
-let (parse_method_descriptor [@deprecated]) = parse_method_descriptor
-let (parse_field_descriptor [@deprecated]) = parse_field_descriptor
 
 let size (t : vtype) : int =
   match t with
