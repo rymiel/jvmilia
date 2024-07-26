3db9f144d3fee63e57aa4be3abc83006a0e80de1
Emilia Kond <emilia@rymiel.space>
2024-07-26 12:20:59 +0300

    Allow silencing bootstrap loader log

--- lib/verify/testclasses.ml
+++ lib/verify/testclasses.ml
@@ -8,21 +8,28 @@ let parse (path : string) : jclass =
 let class_base_dir = "class/"
 let class_sub_dirs = [ "lib/"; "test/"; "extern/" ] (* in order of priority *)
 
+let silent =
+  match Sys.getenv_opt "JVMILIA_BOOTSTRAP_LOG" with
+  | Some "silent" -> true
+  | Some _ | None -> false
+
 let test_loader (name : string) : jclass =
   let make_path subdir = (class_base_dir ^ subdir ^ name ^ ".class", subdir) in
   let candidate_paths = List.map make_path class_sub_dirs in
-  Printf.printf "\027[33m! bootstrap loading %S\027[0m..." name;
+  if not silent then
+    Printf.printf "\027[33m! bootstrap loading %S\027[0m..." name;
   let find_class (path, subdir) =
     if Sys.file_exists path then Some (parse path, subdir) else None
   in
   let found_class = List.find_map find_class candidate_paths in
   match found_class with
   | Some (cls, path) ->
-      Printf.printf "\t\027[33mfound in %S\027[0m\n" path;
+      if not silent then Printf.printf "\t\027[33mfound in %S\027[0m\n" path;
       cls
   | None ->
       Printf.printf
-        "\n\027[31mClass %S not found. Searched the following locations: %s\027[0m\n"
+        "\n\
+         \027[31mClass %S not found. Searched the following locations: %s\027[0m\n"
         name
         (String.concat ", "
            (List.map
